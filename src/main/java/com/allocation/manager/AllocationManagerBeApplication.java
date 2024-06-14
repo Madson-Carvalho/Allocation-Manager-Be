084909f20a5914f00c04a27d9524fb8e8979a59c
madson_carvalho <madsonraynnon@gmail.com>
2024-06-14 19:35:16 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/com/allocation/manager/AllocationManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.allocation.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocationManagerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allocation/manager/AllocationManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allocation.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocationManagerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
