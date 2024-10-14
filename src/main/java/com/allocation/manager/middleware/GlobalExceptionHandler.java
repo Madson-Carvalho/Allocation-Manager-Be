44b16ac62e05b28b59611d2be37c124cf816faac
GabriellMonteiro <gabrielbrava@outlook.com.br>
2024-10-14 20:30:20 -0300

    Implementa middleware para capturar e tratar exceções em requests

---
+++ src/main/java/com/allocation/manager/middleware/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.allocation.manager.middleware;
+
+import com.allocation.manager.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+    }
+}
